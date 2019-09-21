ce911f3876f5906b46999d31f46eea6f73aeafa0
Plasmatic <moses@mosesxu.net>
2019-09-21 00:17:31 -0400

    updated template :)

--- template.h
+++ template.h
@@ -1,26 +1,32 @@
 //begintemplate template
+#pragma region
 #include <bits/stdc++.h>
-
 using namespace std;
-
 // Common DS shorteners
 using ll = long long; using ull = unsigned long long;
 using pii = pair<int, int>; using pll = pair<ll, ll>;
 template <typename T> using vec = vector<T>;
-template <typename K, typename V> using mp = unordered_map<K, V>;
-template <typename K> using us = unordered_set<K>;
+template <typename K, typename V> using um = unordered_map<K, V>; template <typename K, typename V> using om = map<K, V>;
+template <typename K> using us = unordered_set<K>; template <typename K> using os = set<K>;
 using vi = vec<int>; using vl = vec<ll>; using vpi = vec<pii>; using vpl = vec<pll>;
 // Shorthand Macros
 #define INF 0x3f3f3f3f
 #define LLINF 0x3f3f3f3f3f3f3f3f
 #define mpr make_pair
 #define pb push_back
+// Shorthand Function Macros
+#define FORI(a, b) for (int i = a; i < b; i++)
+#define FORJ(a, b) for (int j = a, j < b; j++)
+#define FORK(a, b) for (int k = a; k < b; k++)
+#define CMPLT(type) bool operator<(const type &a, const type &b)
+#define CMPGT(type) bool operator>(const type &a, const type &b)
+#define CMPFN(type) bool operator()(const type &a, const type &b)
 // Shorthand Functions
 template<typename T> inline void maxa(T& st, T v) { st = max(st, v); }
 template<typename T> inline void mina(T& st, T v) { st = min(st, v); }
 // Out operators and printing for arrays and vectors
 template <typename T> ostream& operator<<(ostream& out,vector<T> iter){out<<"[";for(auto t:iter){out<<t<<", ";}out<<"]";return out;}
-template <typename T> void printDebug(T *arr,int sz){cout<<"[";for(int i=0;i<sz;i++){cout<<arr[i]<<", "; } cout<<"]\n";}
+template <typename T> void debugArray(T *arr,int sz){cout<<"[";for(int i=0;i<sz;i++){cout<<arr[i]<<", "; } cout<<"]\n";}
 template <typename T> void printArray(T *arr,int sz){for(int i=0;i<sz;i++){cout<<arr[i]<<" "; } cout<<"\n";}
 #define OUT_OPERATOR(type, propa, propb) ostream& operator<<(ostream& out,type obj){out<<"("<<#propa<<"="<<obj. propa<<", "<<#propb<<"="<<obj. propb<<")";return out;}
 // I/O Operations
@@ -36,6 +42,11 @@ void debug_(){cout<<endl;}
 template<typename F, typename... R>
 void debug_(F f,R... r){int bc=0;while (bc != 0 || dnms_[di_] != ','){if (dnms_[di_] == '(') {bc++;}else if (dnms_[di_] == ')') {bc--;}cout << dnms_[di_++];}di_++;cout << ": " << f << ",";debug_(r...);}
 #define debug(...) do{dnms_=#__VA_ARGS__+co_,di_=0,debug_(__VA_ARGS__);}while(0)
+#pragma endregion
+
+// Custom Types
+using Arr = int[1];
+
 
 int main(){
     ios_base::sync_with_stdio(false);
